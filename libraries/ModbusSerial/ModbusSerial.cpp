462d1cd325d52e94f718b94456a5529467d03679
Oğuz Kaan Yüksel <okyksl@gmail.com>
2017-12-10 10:10:20 +0300

    feat(serial): configurate port

--- libraries/ModbusSerial/ModbusSerial.cpp
+++ libraries/ModbusSerial/ModbusSerial.cpp
@@ -4,3 +4,24 @@ ModbusSerial::ModbusSerial(uint8_t slave, const uint16_t* size) : Modbus(slave, 
 ModbusSerial::ModbusSerial(uint8_t slave, const uint16_t* size, uint32_t codes, ResponseCallback callback) :
     Modbus(slave, size, codes, callback) {}
 ModbusSerial::~ModbusSerial() {}
+
+void ModbusSerial::config(HardwareSerial* port, uint32_t baud, uint16_t format, uint16_t pin = -1) {
+    _port = port;
+    _pin = pin;
+    (*port).begin(baud, format);
+    
+    delay(2000);
+    
+    if (_pin >= 0) {
+        pinMode(_pin, OUTPUT);
+        digitalWrite(_pin, LOW);
+    }
+    
+    if (baud > 19200) {
+        _tin = 750;
+        _tout = 1750;
+    } else {
+        _tin = 15000000 / baud;
+        _tout = 35000000 / baud;
+    }
+}
--- libraries/ModbusSerial/ModbusSerial.h
+++ libraries/ModbusSerial/ModbusSerial.h
@@ -8,11 +8,22 @@
 #endif
 
 class ModbusSerial : public Modbus {
+private:
+    // Port configurations
+    Stream*   _port;
+    uint32_t  _baud;
+    uint16_t  _format;
+    uint16_t  _pin;
+    uint16_t  _tin;
+    uint16_t  _tout;
+    
 public:
     ModbusSerial(uint8_t slave, const uint16_t* size);
     ModbusSerial(uint8_t slave, const uint16_t* size, uint32_t codes);
     ModbusSerial(uint8_t slave, const uint16_t* size, uint32_t codes, ResponseCallback callback);
     ~ModbusSerial();
+    
+    void config(HardwareSerial* port, uint32_t baud, uint16_t format, uint16_t pin = -1); // configurate port
 };
 
 #endif /* ModbusSerial_h */
